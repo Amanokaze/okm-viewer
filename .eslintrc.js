b81f222f932b835519279a629719c308387b4b6d
amanokaze <oniamano@gmail.com>
2022-08-05 11:34:35 +0900

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    plugins: [
+      'svelte3'
+    ],
+    overrides: [
+      {
+        files: ['*.svelte'],
+        processor: 'svelte3/svelte3'
+      }
+    ],
+    "rules": {
+    }
+};
